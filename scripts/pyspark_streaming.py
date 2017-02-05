da7445c5a6078801fb146dabc284ac8f13d658b1
Sven Kreiss <me@svenkreiss.com>
2017-02-04 19:41:57 -0500

    verify window

--- scripts/pyspark_streaming.py
+++ scripts/pyspark_streaming.py
@@ -26,6 +26,13 @@ def save_text(ssc):
      .saveAsTextFiles('scripts/textout/'))
 
 
+def window(ssc):
+    (ssc
+     .queueStream([[1], [2], [3], [4], [5], [6]])
+     .window(3)
+     .foreachRDD(lambda rdd: print('>>>>>>>>>', rdd.collect())))
+
+
 if __name__ == '__main__':
     sc = pyspark.SparkContext()
     ssc = pyspark.streaming.StreamingContext(sc, 1)
@@ -33,7 +40,8 @@ if __name__ == '__main__':
     # simple_queue(ssc)
     # simple_queue_count(ssc)
     # simple_queue_one_at_a_time(ssc)
-    save_text(ssc)
+    # save_text(ssc)
+    window(ssc)
 
     ssc.start()
     time.sleep(3.0)
