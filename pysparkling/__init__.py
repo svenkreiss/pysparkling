dd66a8582f45a96e82d799273006764876df5ec2
Sven Kreiss <me@svenkreiss.com>
2017-07-30 17:13:15 -0400

    version 0.4.6

--- pysparkling/__init__.py
+++ pysparkling/__init__.py
@@ -1,7 +1,7 @@
 """pysparkling module"""
 # flake8: noqa
 
-__version__ = '0.4.5'
+__version__ = '0.4.6'
 
 from .exceptions import (FileAlreadyExistsException,
                          ConnectionException)
