5ffcf9d0311d2fb27464ddcd4e58b0723059b14f
Sven Kreiss <me@svenkreiss.com>
2019-05-11 12:07:19 +0200

    fix

--- pysparkling/tests/test_streaming_files.py
+++ pysparkling/tests/test_streaming_files.py
@@ -18,7 +18,7 @@ class TextFile(tornado.testing.AsyncTestCase):
 
         ssc.start()
         ssc.awaitTermination(timeout=0.3)
-        self.assertEqual(sum(result), 22)
+        self.assertEqual(sum(result), 43)
 
     def test_save(self):
         sc = pysparkling.Context()
@@ -71,4 +71,4 @@ class BinaryFile(tornado.testing.AsyncTestCase):
 
         ssc.start()
         ssc.awaitTermination(timeout=0.3)
-        self.assertEqual(sum(result), 28)
+        self.assertEqual(sum(result), 52)
