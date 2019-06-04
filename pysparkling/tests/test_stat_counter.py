4155899e2b20291dd41119150d898ba6138df2b1
tools4origins <tools4origins@gmail.com>
2019-06-04 22:40:32 +0200

    Add row and column stat helpers

--- pysparkling/tests/test_stat_counter.py
+++ pysparkling/tests/test_stat_counter.py
@@ -1,6 +1,7 @@
 from __future__ import division
 
 import pysparkling
+from pysparkling.stat_counter import ColumnStatHelper
 
 
 def test_mean():
@@ -9,5 +10,26 @@ def test_mean():
     assert sum(d) / len(d) == s.mean()
 
 
+def test_column_stat_helper():
+    """
+    Expected quantile values come from use of org.apache.spark.sql.catalyst.util.QuantileSummaries
+    """
+    helper = ColumnStatHelper()
+    for i in range(1, 100001):
+        helper.merge(i)
+    helper.finalize()
+    assert helper.count == 100000
+    assert helper.min == 1
+    assert helper.max == 100000
+    assert helper.mean == 50000.5
+    assert helper.stddev == 28867.65779668774  # sample standard deviation
+    assert helper.get_quantile(0) == 1
+    assert helper.get_quantile(0.25) == 24998
+    assert helper.get_quantile(0.5) == 50000
+    assert helper.get_quantile(0.75) == 74993
+    assert helper.get_quantile(1) == 100000
+
+
 if __name__ == '__main__':
     test_mean()
+    test_column_stat_helper()
--- pysparkling/utils.py
+++ pysparkling/utils.py
@@ -1,7 +1,11 @@
 import datetime
-import re
 import sys
+import itertools
+import random
+import re
 from operator import itemgetter
+
+from pyspark import Row
 
 
 WILDCARD_START_PATTERN = re.compile(r'(?P<previous_character>^|[^\\])(?P<wildcard_start>[*?[])')
@@ -40,15 +44,9 @@ def parse_file_uri(expr):
     t = Tokenizer(expr)
     scheme = t.next('://')
     domain = t.next('/')
-    wildcard_match = next(WILDCARD_START_PATTERN.finditer(t.expression), None)
-    if wildcard_match is not None:
-        first_pattern_position = wildcard_match.start("wildcard_start")
-    else:
-        first_pattern_position = len(t.expression)
-
-    last_slash_position = t.expression.rfind('/', 0, first_pattern_position)
-    folder_path = '/' + t.expression[:last_slash_position+1]
-    file_pattern = t.expression[last_slash_position+1:]
+    last_slash_position = t.expression.rfind('/')
+    folder_path = '/' + t.expression[:last_slash_position + 1]
+    file_pattern = t.expression[last_slash_position + 1:]
 
     return scheme, domain, folder_path, file_pattern
 
@@ -57,50 +55,149 @@ def format_file_uri(scheme, domain, *local_path_components):
     return '{0}://{1}{2}'.format(scheme, domain, "/".join(local_path_components))
 
 
-def strhash(string):
+def reservoir_sample_and_size(iterable, k, seed):
     """
-    Old python hash function as described in PEP 456, excluding prefix, suffix and mask.
+        Returns a sample of k items of iterable and its original size
+        If the iterable contains less than k items the sample is a list of those items
+        Algorithm used is reservoir sampling.
 
-    :param string: string to hash
-    :return: hash
+        :rtype list
     """
-    if string == "":
+    random.seed(seed)
+
+    # Put the first k elements in the reservoir.
+    reservoir = list(itertools.islice(iterable, k))
+
+    # If we have consumed all the elements, return them. Otherwise do the replacement.
+    l = len(reservoir)
+    if l < k:
+        return reservoir, l
+
+    # If input size > k, continue the sampling process.
+    for l, item in enumerate(iterable, start=k + 1):
+        # There are k elements in the reservoir, and the l-th element has been
+        # consumed. It should be chosen with probability k/l. The expression
+        # below is a random int chosen uniformly from [0, l)
+        replacementIndex = random.randint(0, l)
+        if replacementIndex < k:
+            reservoir[replacementIndex.toInt] = item
+
+    return reservoir, l
+
+
+def compute_weighted_percentiles(weighted_values, number_of_percentiles, key=lambda x: x):
+    """
+    Compute weighted percentiles from a list of values and weights.
+
+    number_of_percentiles evenly distributed percentiles values will be returned,
+    including the 0th (minimal value) and the 100th (maximal value).
+
+    A custom key function can be supplied to customize the sort order
+
+    :type weighted_values: list of tuple
+    :type number_of_percentiles: int
+    :type key: function
+
+    Examples with 0th, 50th and 100th percentiles:
+    >>> compute_weighted_percentiles([(2, 0.2), (1, 0.1), (3, 0.7)], 3)
+    [1, 3, 3]
+    >>> compute_weighted_percentiles([(1, 10), (2, 20), (3, 20)], 3)
+    [1, 2, 3]
+    >>> compute_weighted_percentiles([(i, 1) for i in range(1, 101)], 1)
+    Traceback (most recent call last):
+     ...
+    ValueError: number_of_percentiles must be at least 2
+    >>> compute_weighted_percentiles([(i, 1) for i in range(1, 101)], 2)
+    [1, 100]
+    >>> compute_weighted_percentiles([(i, 1) for i in range(1, 101)], 3)
+    [1, 50, 100]
+    >>> compute_weighted_percentiles([(i, 1) for i in range(1, 101)], 4)
+    [1, 34, 67, 100]
+    >>> compute_weighted_percentiles([(i, 1) for i in range(1, 101)], 5)
+    [1, 25, 50, 75, 100]
+    >>> compute_weighted_percentiles([
+    ...   ((1, "b"), 10),
+    ...   ((2, "c"), 20),
+    ...   ((3, "a"), 20)
+    ... ], 3, key=lambda row: row[1])
+    [(3, 'a'), (1, 'b'), (2, 'c')]
+    """
+    if number_of_percentiles == 1:
+        raise ValueError("number_of_percentiles must be at least 2")
+
+    ordered_values = sorted(weighted_values, key=lambda weighted_value: key(weighted_value[0]))
+    total_weight = sum(weight for value, weight in ordered_values)
+
+    bounds = []
+    cumulative_weight = 0
+    for value, weight in ordered_values:
+        cumulative_weight += weight
+        while len(bounds) / (number_of_percentiles - 1) <= cumulative_weight / total_weight:
+            bounds.append(value)
+
+    return bounds
+
+
+def get_keyfunc(cols):
+    """
+    Return a function that maps a row to a tuple of some of its columns values
+    """
+
+    def key(row):
+        """
+        Returns a tuple of a row column values in the requested column order
+        """
+        return tuple(row[col] for col in cols)
+
+    return key
+
+
+def row_from_keyed_values(keyed_values):
+    # Preserve Row column order which is modified when calling Row(dict)
+    new_row = Row(*[value for key, value in keyed_values])
+    new_row.__fields__ = [key for key, value in keyed_values]
+    return new_row
+
+
+FULL_WIDTH_REGEX = fullWidthRegex = re.compile(
+    "[" +
+    "\u1100-\u115F" +
+    "\u2E80-\uA4CF" +
+    "\uAC00-\uD7A3" +
+    "\uF900-\uFAFF" +
+    "\uFE10-\uFE19" +
+    "\uFE30-\uFE6F" +
+    "\uFF00-\uFF60" +
+    "\uFFE0-\uFFE6" +
+    "]"
+)
+
+
+def str_half_width(string):
+    """
+    Compute string length with full width characters counting for 2 normal ones
+    """
+    if string is None:
         return 0
-
-    x = ord(string[0]) << 7
-    for c in string[1:]:
-        x = ((1000003 * x) ^ ord(c)) & (1 << 32)
-    x = (x ^ len(string))
-    return x
+    if not isinstance(string, str):
+        string = str(string)
+    return len(string) + len(FULL_WIDTH_REGEX.findall(string))
 
 
-def portable_hash(x):
+def pad_cell(cell, truncate, col_width):
     """
-    This function returns consistent hash code for builtin types, especially
-    for None and tuple with None.
-    The algorithm is similar to that one used by CPython 2.7
-    >>> portable_hash(None)
-    0
-    >>> portable_hash((None, 1)) & 0xffffffff
-    219750521
+    if (truncate > 0) {
+    StringUtils.leftPad(cell, colWidths(i) - Utils.stringHalfWidth(cell) + cell.length)
+  } else {
+    StringUtils.rightPad(cell, colWidths(i) - Utils.stringHalfWidth(cell) + cell.length)
+  }
     """
-
-    if x is None:
-        return 0
-    if isinstance(x, list):
-        return portable_hash(tuple(x))
-    if isinstance(x, tuple):
-        h = 0x345678
-        for i in x:
-            h ^= portable_hash(i)
-            h *= 1000003
-            h &= sys.maxsize
-        h ^= len(x)
-        if h == -1:
-            h = -2
-        return int(h)
-    if isinstance(x, str):
-        return strhash(x)
-    if isinstance(x, datetime.datetime):
-        return portable_hash(x.timetuple())
-    return hash(x)
+    if cell is None:
+        cell = "null"
+    if not isinstance(cell, str):
+        cell = str(cell)
+    cell_width = col_width - str_half_width(cell) + len(cell)
+    if truncate > 0:
+        return cell.rjust(cell_width)
+    else:
+        return cell.ljust(cell_width)
