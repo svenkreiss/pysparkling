ce1d4026efa5d50f83e561d547c7ed0904e76b1f
tools4origins <tools4origins@gmail.com>
2019-06-13 22:46:27 +0200

    Expose SparkSession in pysparkling root module

--- pysparkling/__init__.py
+++ pysparkling/__init__.py
@@ -10,11 +10,12 @@ from .broadcast import Broadcast
 from .accumulators import  Accumulator, AccumulatorParam
 from .stat_counter import StatCounter
 from .cache_manager import CacheManager, TimedCacheManager
+from .sql.session import SparkSession
 
 from . import fileio
 from . import streaming
 from . import exceptions
 
 __all__ = ['RDD', 'Context', 'Broadcast', 'StatCounter', 'CacheManager', 'Row',
-           'TimedCacheManager',
+           'TimedCacheManager', 'SparkSession',
            'exceptions', 'fileio', 'streaming']
