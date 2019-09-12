ad680223d1c770058a576d729d70ee6963f113e5
tools4origins <tools4origins@gmail.com>
2019-09-12 22:08:36 +0200

    Move InternalReader to internal module

--- pysparkling/sql/internal_utils/readers.py
+++ pysparkling/sql/internal_utils/readers.py
@@ -3,10 +3,34 @@ import json
 
 from pysparkling.fileio import TextFile, File
 from pysparkling.sql.casts import *
+from pysparkling.sql.internal_utils.readwrite import OptionUtils, to_option_stored_value
 from pysparkling.sql.internals import DataFrameInternal
 from pysparkling.sql.schema_utils import infer_schema_from_rdd
 from pysparkling.sql.utils import AnalysisException
 from pysparkling.utils import row_from_keyed_values
+
+
+class InternalReader(OptionUtils):
+    def schema(self, schema):
+        self._schema = schema
+
+    def option(self, k, v):
+        self._options[k.lower()] = to_option_stored_value(v)
+
+    def __init__(self, spark):
+        """
+
+        :type spark: pysparkling.sql.session.SparkSession
+        """
+        self._spark = spark
+        self._options = {}
+        self._schema = None
+
+    def csv(self, paths):
+        return CSVReader(self._spark, paths, self._schema, self._options).read()
+
+    def json(self, paths):
+        return JSONReader(self._spark, paths, self._schema, self._options).read()
 
 
 class CSVReader(object):
