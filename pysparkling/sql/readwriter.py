0bd4a806caccc08bf2cd7f57222a8fbbbccc34c7
tools4origins <tools4origins@gmail.com>
2019-09-12 22:08:36 +0200

    Move InternalReader to internal module

--- pysparkling/sql/readwriter.py
+++ pysparkling/sql/readwriter.py
@@ -1,6 +1,6 @@
 import sys
 from pysparkling import RDD
-from pysparkling.sql.internal_utils.readers import CSVReader, JSONReader
+from pysparkling.sql.internal_utils.readers import CSVReader, JSONReader, InternalReader
 from pysparkling.sql.internal_utils.readwrite import OptionUtils, to_option_stored_value
 from pysparkling.sql.internal_utils.writers import CSVWriter
 from pysparkling.sql.utils import IllegalArgumentException
@@ -86,29 +86,6 @@ class DataFrameReader(OptionUtils):
             return self._df(self._jreader.json(path.collect()))
         else:
             raise TypeError("path can be only string, list or RDD")
-
-
-class InternalReader(OptionUtils):
-    def schema(self, schema):
-        self._schema = schema
-
-    def option(self, k, v):
-        self._options[k.lower()] = to_option_stored_value(v)
-
-    def __init__(self, spark):
-        """
-
-        :type spark: pysparkling.sql.session.SparkSession
-        """
-        self._spark = spark
-        self._options = {}
-        self._schema = None
-
-    def csv(self, paths):
-        return CSVReader(self._spark, paths, self._schema, self._options).read()
-
-    def json(self, paths):
-        return JSONReader(self._spark, paths, self._schema, self._options).read()
 
 
 class InternalWriter(object):
---
+++ pysparkling/sql/tests/test_write.py
@@ -0,0 +1,85 @@
+import datetime
+import os
+import shutil
+from unittest import TestCase
+
+from pysparkling import Context, Row
+from pysparkling.sql.session import SparkSession
+from pysparkling.sql.utils import AnalysisException
+
+spark = SparkSession(Context())
+
+
+def get_folder_content(folder_path):
+    folder_content = {}
+    for root, dirs, files in os.walk(folder_path):
+        relative_path = root[len(folder_path):]
+        for file in files:
+            file_path = os.path.join(root, file)
+            with open(file_path, 'r') as file_content:
+                folder_content[os.path.join(relative_path, file)] = file_content.readlines()
+    return folder_content
+
+
+class DataFrameWriterTests(TestCase):
+    @staticmethod
+    def clean():
+        if os.path.exists(".tmp"):
+            shutil.rmtree(".tmp")
+
+    def setUp(self):
+        self.clean()
+
+    def tearDown(self):
+        self.clean()
+
+    def test_write_to_csv(self):
+        df = spark.createDataFrame(
+            [Row(age=2, name='Alice', time=datetime.datetime(2017, 1, 1), ),
+             Row(age=5, name='Bob', time=datetime.datetime(2014, 3, 2))]
+        )
+        df.write.csv(".tmp/wonderland/")
+        self.assertDictEqual(
+            get_folder_content(".tmp/wonderland"),
+            {'_SUCCESS': [],
+             'part-00000-3432532549516755848.csv': [
+                 '2,Alice\n',
+                 '5,Bob\n'
+             ]}
+        )
+
+    def test_write_to_csv_with_custom_options(self):
+        df = spark.createDataFrame(
+            [
+                Row(age=2, name='Alice', occupation=None),
+                Row(age=5, name='Bob', occupation=""),
+            ]
+        )
+        df.write.csv(".tmp/wonderland/", sep="^", emptyValue="", nullValue="null", header=True)
+        self.assertDictEqual(
+            get_folder_content(".tmp/wonderland"),
+            {'_SUCCESS': [],
+             'part-00000-384289621784791593.csv': [
+                 'age^name^occupation\n',
+                 '2^Alice^null\n',
+                 '5^Bob^\n'
+             ]}
+        )
+
+    def test_write_to_csv_fail_when_overwrite(self):
+        df = spark.createDataFrame(
+            [Row(age=2, name='Alice'),
+             Row(age=5, name='Bob')]
+        )
+        df.write.csv(".tmp/wonderland/")
+        with self.assertRaises(AnalysisException) as ctx:
+           df.write.csv(".tmp/wonderland/")
+        self.assertEqual(ctx.exception.args[0], 'path .tmp/wonderland already exists.;')
+        self.assertDictEqual(
+            get_folder_content(".tmp/wonderland"),
+            {'_SUCCESS': [],
+             'part-00000-3432532549516755848.csv': [
+                 '2,Alice\n',
+                 '5,Bob\n'
+             ]}
+        )
