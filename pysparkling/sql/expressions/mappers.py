a6685a4a43b0275f4d46512058dd2baa8f1d31c0
tools4origins <tools4origins@gmail.com>
2019-10-17 20:23:33 +0200

    Fix numeric operations

--- pysparkling/sql/expressions/mappers.py
+++ pysparkling/sql/expressions/mappers.py
@@ -90,8 +90,8 @@ class NullSafeBinaryOperation(BinaryOperation):
         type_1 = value_1.__class__
         type_2 = value_2.__class__
         if type_1 == type_2 or (
-                isinstance(type_1, NumericType) and
-                isinstance(type_2, NumericType)
+                isinstance(value_1, (int, float)) and
+                isinstance(value_2, (int, float))
         ):
             return self.unsafe_operation(value_1, value_2)
 
