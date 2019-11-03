40c87bf1f163a2c3b126c7b98173c8ee4a6fe790
tools4origins <tools4origins@gmail.com>
2019-11-03 19:09:39 +0100

    Implement grouping

--- pysparkling/sql/expressions/mappers.py
+++ pysparkling/sql/expressions/mappers.py
@@ -1306,3 +1306,15 @@ class GroupingID(Expression):
         return "grouping_id({0})".format(
             ", ".join(str(col) for col in self.columns)
         )
+
+
+class Grouping(UnaryExpression):
+    def eval(self, row, schema):
+        metadata = row.get_metadata()
+        if metadata is None or "grouping" not in metadata:
+            raise AnalysisException("grouping_id() can only be used with GroupingSets/Cube/Rollup")
+        pos = self.column.find_position_in_schema(schema)
+        return int(metadata["grouping"][pos])
+
+    def __str__(self):
+        return "grouping({0})".format(self.column)
