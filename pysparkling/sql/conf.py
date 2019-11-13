d3309ef3eee47ff18c0b99e80dfd4e1be97e6a4f
tools4origins <tools4origins@gmail.com>
2019-11-13 22:09:59 +0100

    Align sql conf code with pylint recommendations

--- pysparkling/sql/conf.py
+++ pysparkling/sql/conf.py
@@ -11,16 +11,15 @@ class RuntimeConfig(object):
         self._conf = {}
 
     def set(self, key, value):
-        self._conf[key]= value
+        self._conf[key] = value
 
     def get(self, key, default=_sentinel):
         self._checkType(key, "key")
         if default is _sentinel:
             return self._conf.get(key)
-        else:
-            if default is not None:
-                self._checkType(default, "default")
-            return self._conf.get(key, default)
+        if default is not None:
+            self._checkType(default, "default")
+        return self._conf.get(key, default)
 
     def unset(self, key):
         del self._conf[key]
@@ -31,6 +30,5 @@ class RuntimeConfig(object):
                             (identifier, obj, type(obj).__name__))
 
     def isModifiable(self, key):
-        # todo: isModifiable is not implemented
-        return self._conf.isModifiable(key)
-
+        raise NotImplementedError("pysparkling does not support yet this feature")
+        # return self._conf.isModifiable(key)
