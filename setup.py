4a45aa4d46f406d4f292f8dda20caba64dd3b921
tools4origins <tools4origins@gmail.com>
2019-11-13 18:42:16 +0100

    Add python-dateutil as a dependency

--- setup.py
+++ setup.py
@@ -36,7 +36,8 @@ setup(
         'boto>=2.36.0',
         'future>=0.15',
         'requests>=2.6.0',
-        'pytz>=2019.3'
+        'pytz>=2019.3',
+        'python-dateutil>=2.8.0'
     ],
     extras_require={
         'hdfs': ['hdfs>=2.0.0'],
