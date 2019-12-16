0eda986755fd38eae328dc746cfd74bd1ea8e696
tools4origins <tools4origins@gmail.com>
2019-12-16 12:12:17 +0100

    Implement RDD.subtractByKey

--- pysparkling/rdd.py
+++ pysparkling/rdd.py
@@ -1716,6 +1716,24 @@ class RDD(object):
             preservesPartitioning=True,
         )
 
+    def subtractByKey(self, other, numPartitions=None):
+        """
+        Return each (key, value) pair in C{self} that has no pair with matching
+        key in C{other}.
+        >>> from pysparkling import Context
+        >>> sc = Context()
+        >>> x = sc.parallelize([("a", 1), ("b", 4), ("b", 5), ("a", 2)])
+        >>> y = sc.parallelize([("a", 3), ("c", None)])
+        >>> sorted(x.subtractByKey(y).collect())
+        [('b', 4), ('b', 5)]
+        """
+
+        def filter_func(pair):
+            _, (val1, val2) = pair
+            return val1 and not val2
+
+        return self.cogroup(other, numPartitions).filter(filter_func).flatMapValues(lambda x: x[0])
+
     def sum(self):
         """sum of all the elements
 
