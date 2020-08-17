06169db18c0e90f138eec92c4fda2fa1a565e2d0
tools4origins <tools4origins@gmail.com>
2020-08-17 21:28:59 +0200

    Implement Divide

--- pysparkling/sql/expressions/operators.py
+++ pysparkling/sql/expressions/operators.py
@@ -25,9 +25,18 @@ class Minus(NullSafeBinaryOperation):
     def __str__(self):
         return "({0} - {1})".format(self.arg1, self.arg2)
 
+
 class Time(NullSafeBinaryOperation):
     def unsafe_operation(self, value1, value2):
         return value1 * value2
 
     def __str__(self):
         return "({0} * {1})".format(self.arg1, self.arg2)
+
+
+class Divide(NullSafeBinaryOperation):
+    def unsafe_operation(self, value1, value2):
+        return value1 / value2 if value2 != 0 else None
+
+    def __str__(self):
+        return "({0} / {1})".format(self.arg1, self.arg2)
