9fd0c9c7a471e4d88707a6f2c0b509e0c7c31fef
tools4origins <tools4origins@gmail.com>
2020-08-17 21:38:37 +0200

    Define SortOrder

---
+++ pysparkling/sql/expressions/orders.py
@@ -0,0 +1,15 @@
+from pysparkling.sql.expressions.expressions import Expression
+
+
+class SortOrder(Expression):
+    sort_order = None
+
+    def __init__(self, column):
+        super(SortOrder, self).__init__(column)
+        self.column = column
+
+    def eval(self, row, schema):
+        return self.column.eval(row, schema)
+
+    def __str__(self):
+        return "{0} {1}".format(self.column, self.sort_order)
