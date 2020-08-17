61222a30fb40e5ee232b2987f3fe3f9e426c74b8
tools4origins <tools4origins@gmail.com>
2020-08-17 21:28:44 +0200

    Implement Time

--- pysparkling/sql/expressions/operators.py
+++ pysparkling/sql/expressions/operators.py
@@ -24,3 +24,10 @@ class Minus(NullSafeBinaryOperation):
 
     def __str__(self):
         return "({0} - {1})".format(self.arg1, self.arg2)
+
+class Time(NullSafeBinaryOperation):
+    def unsafe_operation(self, value1, value2):
+        return value1 * value2
+
+    def __str__(self):
+        return "({0} * {1})".format(self.arg1, self.arg2)
