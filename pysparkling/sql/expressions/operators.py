07d2f3accf93e12daba44cdccf6d405b87742fa5
tools4origins <tools4origins@gmail.com>
2020-08-17 21:29:33 +0200

    Implement Pow

--- pysparkling/sql/expressions/operators.py
+++ pysparkling/sql/expressions/operators.py
@@ -49,3 +49,11 @@ class Mod(NullSafeBinaryOperation):
     def __str__(self):
         return "({0} % {1})".format(self.arg1, self.arg2)
 
+
+class Pow(NullSafeBinaryOperation):
+    def unsafe_operation(self, value1, value2):
+        return float(value1 ** value2)
+
+    def __str__(self):
+        return "POWER({0}, {1})".format(self.arg1, self.arg2)
+
