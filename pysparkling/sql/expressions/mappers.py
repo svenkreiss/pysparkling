18fe649df424d4b39cbfa08c85cd573bb531c44f
tools4origins <tools4origins@gmail.com>
2020-08-17 21:50:30 +0200

    Implement StarOperator Expression

---
+++ pysparkling/sql/expressions/mappers.py
@@ -0,0 +1,16 @@
+from pysparkling.sql.expressions.expressions import Expression
+
+
+class StarOperator(Expression):
+    @property
+    def may_output_multiple_cols(self):
+        return True
+
+    def output_fields(self, schema):
+        return schema.fields
+
+    def eval(self, row, schema):
+        return [row[col] for col in row.__fields__]
+
+    def __str__(self):
+        return "*"
