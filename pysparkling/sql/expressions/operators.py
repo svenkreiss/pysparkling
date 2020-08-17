78f1f3de6dd11dc27b2d79674380315f4c8b9871
tools4origins <tools4origins@gmail.com>
2020-08-17 21:28:30 +0200

    Implement Minus

--- pysparkling/sql/expressions/operators.py
+++ pysparkling/sql/expressions/operators.py
@@ -16,3 +16,11 @@ class Add(NullSafeBinaryOperation):
 
     def __str__(self):
         return "({0} + {1})".format(self.arg1, self.arg2)
+
+
+class Minus(NullSafeBinaryOperation):
+    def unsafe_operation(self, value1, value2):
+        return value1 - value2
+
+    def __str__(self):
+        return "({0} - {1})".format(self.arg1, self.arg2)
