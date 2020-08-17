c0b3ee7304033db45d2ccf68ce29aab9757c023d
tools4origins <tools4origins@gmail.com>
2020-08-17 21:32:51 +0200

    Implement BitwiseOr

--- pysparkling/sql/expressions/operators.py
+++ pysparkling/sql/expressions/operators.py
@@ -131,3 +131,16 @@ class Invert(UnaryExpression):
 
     def __str__(self):
         return "(NOT {0})".format(self.column)
+
+
+class BitwiseOr(Expression):
+    def __init__(self, arg1, arg2):
+        super(BitwiseOr, self).__init__(arg1, arg2)
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def eval(self, row, schema):
+        return self.arg1.eval(row, schema) | self.arg2.eval(row, schema)
+
+    def __str__(self):
+        return "({0} | {1})".format(self.arg1, self.arg2)
