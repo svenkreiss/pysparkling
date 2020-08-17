a5bd7db13afb88a99369bc301bc445645d1136cd
tools4origins <tools4origins@gmail.com>
2020-08-17 21:15:52 +0200

    Add merge stats logic for aggregations

--- pysparkling/sql/expressions/expressions.py
+++ pysparkling/sql/expressions/expressions.py
@@ -60,3 +60,31 @@ class Expression(object):
                 child.expr.recursive_merge(row, schema)
             elif isinstance(child, (list, set, tuple)):
                 Expression.children_merge(child, row, schema)
+
+    def mergeStats(self, other, schema):
+        pass
+
+    def recursive_merge_stats(self, other, schema):
+        # todo: Add logic
+        # # Top level import would cause cyclic dependencies
+        # # pylint: disable=import-outside-toplevel
+        # from pysparkling.sql.expressions.operators import Alias
+        # if isinstance(other.expr, Alias):
+        #     self.recursive_merge_stats(other.expr.expr, schema)
+        # else:
+        self.mergeStats(other.expr, schema)
+        self.children_merge_stats(self.children, other, schema)
+
+    @staticmethod
+    def children_merge_stats(children, other, schema):
+        # # Top level import would cause cyclic dependencies
+        # # pylint: disable=import-outside-toplevel
+        # from pysparkling.sql.column import Column
+        for child in children:
+            if isinstance(child, Expression):
+                child.recursive_merge_stats(other, schema)
+            # todo: elif isinstance(child, Column) and isinstance(child.expr, Expression):
+            elif isinstance(child.expr, Expression):
+                child.expr.recursive_merge_stats(other, schema)
+            elif isinstance(child, (list, set, tuple)):
+                Expression.children_merge_stats(child, other, schema)
