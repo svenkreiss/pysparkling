8880d1389ad815bfc72c51f83937be60fbe3963d
tools4origins <tools4origins@gmail.com>
2020-08-17 19:20:18 +0200

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -58,10 +58,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: PyPy',
     ]
 )
