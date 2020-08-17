696bbecce574bb4c4784a93f19e3accdd1aed9ba
tools4origins <tools4origins@gmail.com>
2020-08-17 21:29:56 +0200

    Implement Pmod

--- pysparkling/sql/expressions/operators.py
+++ pysparkling/sql/expressions/operators.py
@@ -57,3 +57,11 @@ class Pow(NullSafeBinaryOperation):
     def __str__(self):
         return "POWER({0}, {1})".format(self.arg1, self.arg2)
 
+
+class Pmod(NullSafeBinaryOperation):
+    def unsafe_operation(self, value1, value2):
+        return value1 % value2
+
+    def __str__(self):
+        return "pmod({0} % {1})".format(self.arg1, self.arg2)
+
