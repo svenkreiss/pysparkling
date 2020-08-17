079030815e89cb021c929a1a4d2dd94dcc5af3ea
tools4origins <tools4origins@gmail.com>
2020-08-17 21:50:44 +0200

    Implement CaseWhen Expression

--- pysparkling/sql/expressions/mappers.py
+++ pysparkling/sql/expressions/mappers.py
@@ -14,3 +14,32 @@ class StarOperator(Expression):
 
     def __str__(self):
         return "*"
+
+
+class CaseWhen(Expression):
+    def __init__(self, conditions, values):
+        super(CaseWhen, self).__init__(conditions, values)
+
+        self.conditions = conditions
+        self.values = values
+
+    def eval(self, row, schema):
+        for condition, function in zip(self.conditions, self.values):
+            condition_value = condition.eval(row, schema)
+            if condition_value:
+                return function.eval(row, schema)
+        return None
+
+    def __str__(self):
+        return "CASE {0} END".format(
+            " ".join(
+                "WHEN {0} THEN {1}".format(condition, value)
+                for condition, value in zip(self.conditions, self.values)
+            )
+        )
+
+    def add_when(self, condition, value):
+        return CaseWhen(
+            self.conditions + [condition],
+            self.values + [value]
+        )
