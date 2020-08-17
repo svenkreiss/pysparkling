93e6f41092b93047c852c222039640dae629c1d5
tools4origins <tools4origins@gmail.com>
2020-08-17 21:45:41 +0200

    Add a utils to find an expr position in a schema

--- pysparkling/sql/expressions/fields.py
+++ pysparkling/sql/expressions/fields.py
@@ -1,3 +1,50 @@
+from pysparkling.sql.types import StructField
+
+from pysparkling.sql.utils import AnalysisException
+
+
+def find_position_in_schema(schema, expr):
+    if isinstance(expr, str):
+        show_id = False
+        field_name = expr
+        matches = set(i for i, field in enumerate(schema.fields) if field_name == field.name)
+    elif isinstance(expr, StructField) and hasattr(expr, "id"):
+        show_id = True
+        field_name = format_field(expr, show_id=show_id)
+        matches = set(i for i, field in enumerate(schema.fields) if expr.id == field.id)
+    else:
+        if isinstance(expr, StructField):
+            expression = "Unbound field {0}".format(expr.name)
+        else:
+            expression = "Expression type '{0}'".format(type(expr))
+
+        raise NotImplementedError(
+            "{0} is not supported. "
+            "As a user you should not see this error, feel free to report a bug at "
+            "https://github.com/svenkreiss/pysparkling/issues".format(expression)
+        )
+
+    return get_checked_matches(matches, field_name, schema, show_id)
+
+
+def get_checked_matches(matches, field_name, schema, show_id):
+    if not matches:
+        raise AnalysisException("Unable to find the column '{0}' among {1}".format(
+            field_name,
+            format_schema(schema, show_id)
+        ))
+
+    if len(matches) > 1:
+        raise AnalysisException(
+            "Reference '{0}' is ambiguous, found {1} columns matching it.".format(
+                field_name,
+                len(matches)
+            )
+        )
+
+    return matches.pop()
+
+
 def format_schema(schema, show_id):
     return [format_field(field, show_id=show_id) for field in schema.fields]
 
