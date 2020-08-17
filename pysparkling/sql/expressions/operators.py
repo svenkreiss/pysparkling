e8662c465d6a0f5e00648bc03db6c635d211c3bf
tools4origins <tools4origins@gmail.com>
2020-08-17 21:27:35 +0200

    Implement Negate

---
+++ pysparkling/sql/expressions/operators.py
@@ -0,0 +1,9 @@
+from pysparkling.sql.expressions.expressions import Expression, UnaryExpression
+
+
+class Negate(UnaryExpression):
+    def eval(self, row, schema):
+        return not self.column.eval(row, schema)
+
+    def __str__(self):
+        return "(- {0})".format(self.column)
