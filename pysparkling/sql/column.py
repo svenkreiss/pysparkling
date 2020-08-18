0a3d7a1dd916cc63230080968a68e6bd34d35317
tools4origins <tools4origins@gmail.com>
2020-08-18 19:23:35 +0200

    Implement Column arithmetic operators

--- pysparkling/sql/column.py
+++ pysparkling/sql/column.py
@@ -1,5 +1,6 @@
 from pysparkling.sql.expressions.literals import Literal
 from pysparkling.sql.expressions.mappers import StarOperator
+from pysparkling.sql.expressions.operators import Negate, Add, Minus, Time, Divide, Mod, Pow
 
 
 class Column(object):
@@ -21,6 +22,31 @@ class Column(object):
 
     def __init__(self, expr):
         self.expr = expr
+
+    # arithmetic operators
+    def __neg__(self):
+        return Column(Negate(self))
+
+    def __add__(self, other):
+        return Column(Add(self, parse_operator(other)))
+
+    def __sub__(self, other):
+        return Column(Minus(self, parse_operator(other)))
+
+    def __mul__(self, other):
+        return Column(Time(self, parse_operator(other)))
+
+    def __div__(self, other):
+        return Column(Divide(self, parse_operator(other)))
+
+    def __truediv__(self, other):
+        return Column(Divide(self, parse_operator(other)))
+
+    def __mod__(self, other):
+        return Column(Mod(self, parse_operator(other)))
+
+    def __pow__(self, power):
+        return Column(Pow(self, parse_operator(power)))
 
 
 def parse_operator(arg):
