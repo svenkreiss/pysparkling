3e3ea475f89ecab4a5690c35b63ff938465be483
tools4origins <tools4origins@gmail.com>
2020-08-18 19:19:58 +0200

    Add basic Column implementation

---
+++ pysparkling/sql/column.py
@@ -0,0 +1,20 @@
+class Column(object):
+    """
+    A column in a DataFrame.
+
+    :class:`Column` instances can be created by::
+
+        # 1. Select a column out of a DataFrame
+
+        df.colName
+        df["colName"]
+
+        # 2. Create from an expression
+        df.colName + 1
+        1 / df.colName
+
+    .. versionadded:: 1.3
+    """
+
+    def __init__(self, expr):
+        self.expr = expr
