932a23fdf3eaff7fd4faf0e7171ce916b503cb97
tools4origins <tools4origins@gmail.com>
2020-08-23 18:39:20 +0200

    Implement DataFrameInternal base object

--- pysparkling/sql/internals.py
+++ pysparkling/sql/internals.py
@@ -1,4 +1,8 @@
-from pysparkling.sql.types import StructType
+from copy import deepcopy
+from functools import partial
+
+from pysparkling.sql.schema_utils import infer_schema_from_rdd
+from pysparkling.sql.types import StructType, create_row
 
 
 class FieldIdGenerator(object):
@@ -38,3 +42,31 @@ class FieldIdGenerator(object):
             if isinstance(field, StructType):
                 cls.unbind_schema(field)
         return schema
+
+
+class DataFrameInternal(object):
+    def __init__(self, sc, rdd, cols=None, convert_to_row=False, schema=None):
+        """
+        :type rdd: RDD
+        """
+        if convert_to_row:
+            if cols is None:
+                cols = ["_c{0}".format(i) for i in range(200)]
+            rdd = rdd.map(partial(create_row, cols))
+
+        self._sc = sc
+        self._rdd = rdd
+        if schema is None and convert_to_row is False:
+            raise NotImplementedError(
+                "Schema cannot be None when creating DataFrameInternal from another. "
+                "As a user you should not see this error, feel free to report a bug at "
+                "https://github.com/svenkreiss/pysparkling/issues"
+            )
+        if schema is not None:
+            self._set_schema(schema)
+        else:
+            self._set_schema(infer_schema_from_rdd(self._rdd))
+
+    def _set_schema(self, schema):
+        bound_schema = FieldIdGenerator.bind_schema(deepcopy(schema))
+        self.bound_schema = bound_schema
