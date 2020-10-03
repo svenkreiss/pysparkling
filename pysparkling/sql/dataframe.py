794ad3485fc08ce4dfa806229eca85749f5cacb4
tools4origins <tools4origins@gmail.com>
2020-10-03 16:49:48 +0200

    Implement DataFrame.exceptAll

--- pysparkling/sql/dataframe.py
+++ pysparkling/sql/dataframe.py
@@ -75,6 +75,39 @@ class DataFrame(object):
     def explain(self, extended=False):
         print("Pysparkling does not provide query execution explanation")
 
+    def exceptAll(self, other):
+        """Return a new :class:`DataFrame` containing rows in this :class:`DataFrame` but
+        not in another :class:`DataFrame` while preserving duplicates.
+
+        This is equivalent to `EXCEPT ALL` in SQL.
+
+        >>> from pysparkling import Context
+        >>> from pysparkling.sql.session import SparkSession
+        >>> spark = SparkSession(Context())
+        >>> df1 = spark.createDataFrame([
+        ...   ("a", 1),
+        ...   ("a", 1),
+        ...   ("a", 1),
+        ...   ("a", 2),
+        ...   ("b", 3),
+        ...   ("c", 4)
+        ... ], ["C1", "C2"])
+        >>> df2 = spark.createDataFrame([("a", 1), ("b", 3)], ["C1", "C2"])
+        >>> df1.exceptAll(df2).show()
+        +---+---+
+        | C1| C2|
+        +---+---+
+        |  a|  1|
+        |  a|  1|
+        |  a|  2|
+        |  c|  4|
+        +---+---+
+
+        Also as standard in SQL, this function resolves columns by position (not by name).
+        """
+        # noinspection PyProtectedMember
+        return DataFrame(self._jdf.exceptAll(other._jdf), self.sql_ctx)
+
     def dropna(self, how='any', thresh=None, subset=None):
         if how is not None and how not in ['any', 'all']:
             raise ValueError("how ('" + how + "') should be 'any' or 'all'")
