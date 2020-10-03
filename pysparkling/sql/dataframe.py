17f51c45c90f2f2337a159fc7e82ced718910ab6
tools4origins <tools4origins@gmail.com>
2020-10-03 16:48:54 +0200

    Implement DataFrame.printSchema

--- pysparkling/sql/dataframe.py
+++ pysparkling/sql/dataframe.py
@@ -69,6 +69,9 @@ class DataFrame(object):
     def schema(self):
         return self._jdf.unbound_schema
 
+    def printSchema(self):
+        print(self.schema.treeString())
+
     def dropna(self, how='any', thresh=None, subset=None):
         if how is not None and how not in ['any', 'all']:
             raise ValueError("how ('" + how + "') should be 'any' or 'all'")
