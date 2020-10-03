74efe5c5056edcddcd4c1eb5b7daddbe3173dce5
tools4origins <tools4origins@gmail.com>
2020-10-03 18:18:27 +0200

    Implement ArrayColumn Expression

--- pysparkling/sql/expressions/arrays.py
+++ pysparkling/sql/expressions/arrays.py
@@ -1,6 +1,18 @@
 from pysparkling.sql.expressions.expressions import Expression
 
 
+class ArrayColumn(Expression):
+    def __init__(self, columns):
+        super(ArrayColumn, self).__init__(columns)
+        self.columns = columns
+
+    def eval(self, row, schema):
+        return [col.eval(row, schema) for col in self.columns]
+
+    def __str__(self):
+        return "array({0})".format(", ".join(str(col) for col in self.columns))
+
+
 class MapFromArraysColumn(Expression):
     def __init__(self, keys, values):
         super(MapFromArraysColumn, self).__init__(keys, values)
