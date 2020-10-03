0d1469330271f68e7e031e17d9de579dda619d3a
tools4origins <tools4origins@gmail.com>
2020-10-03 16:53:27 +0200

    Clarify DataFrame withWatermark implementation status

--- pysparkling/sql/dataframe.py
+++ pysparkling/sql/dataframe.py
@@ -159,6 +159,9 @@ class DataFrame(object):
     def localCheckpoint(self, eager=True):
         raise NotImplementedError("Streaming is not supported in PySparkling")
 
+    def withWatermark(self, eventTime, delayThreshold):
+        raise NotImplementedError("Streaming is not supported in PySparkling")
+
     def dropna(self, how='any', thresh=None, subset=None):
         if how is not None and how not in ['any', 'all']:
             raise ValueError("how ('" + how + "') should be 'any' or 'all'")
