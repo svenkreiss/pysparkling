2f81c5194678af6fc3cecb025e1b25697430072d
tools4origins <tools4origins@gmail.com>
2020-10-03 16:49:22 +0200

    Clarify DataFrame.explain status

--- pysparkling/sql/dataframe.py
+++ pysparkling/sql/dataframe.py
@@ -72,6 +72,9 @@ class DataFrame(object):
     def printSchema(self):
         print(self.schema.treeString())
 
+    def explain(self, extended=False):
+        print("Pysparkling does not provide query execution explanation")
+
     def dropna(self, how='any', thresh=None, subset=None):
         if how is not None and how not in ['any', 'all']:
             raise ValueError("how ('" + how + "') should be 'any' or 'all'")
