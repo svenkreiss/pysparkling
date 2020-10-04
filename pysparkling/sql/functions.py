97b6e5a3779cd9af5eb52aff47bb53a2120a4bc7
tools4origins <tools4origins@gmail.com>
2020-10-04 15:57:00 +0200

    Implement map_keys function

--- pysparkling/sql/functions.py
+++ pysparkling/sql/functions.py
@@ -19,7 +19,7 @@ from pysparkling.sql.expressions.mappers import CaseWhen, Rand, CreateStruct, Gr
     Bin, Cbrt, Ceil, Conv, Cos, Cosh, Exp, ExpM1, Factorial, Floor, Greatest, Hex, Unhex, Hypot, Least, Log, Log10, \
     Log1p, Log2, Rint, Round, Bround, Signum, Sin, Sinh, Tan, Tanh, ToDegrees, ToRadians, Ascii, Base64, ConcatWs, \
     FormatNumber, Length, Lower, RegExpExtract, RegExpReplace, UnBase64, StringSplit, SubstringIndex, Upper, Concat, \
-    Reverse
+    Reverse, MapKeys
 from pysparkling.sql.expressions.literals import Literal
 from pysparkling.sql.expressions.operators import IsNull, BitwiseNot, Pow, Pmod, Substring
 from pysparkling.sql.expressions.strings import InitCap, StringInStr, Levenshtein, StringLocate, StringLPad, \
@@ -2286,3 +2286,10 @@ def array_repeat(e, count):
     :rtype: Column
     """
     return col(ArrayRepeat(parse(e), count))
+
+
+def map_keys(e):
+    """
+    :rtype: Column
+    """
+    return col(MapKeys(e))
