76b0dd91d0e8ba9bc3a5be5cb213a0342d267dd6
tools4origins <tools4origins@gmail.com>
2020-10-04 15:40:17 +0200

    Implement current_timestamp function

--- pysparkling/sql/functions.py
+++ pysparkling/sql/functions.py
@@ -7,7 +7,7 @@ from pysparkling.sql.expressions.aggregate.covariance_aggregations import Corr, 
 from pysparkling.sql.expressions.aggregate.stat_aggregations import Count, Avg, Kurtosis, Max, Min, Skewness, \
     StddevSamp, StddevPop, Sum, VarSamp, VarPop
 from pysparkling.sql.expressions.arrays import ArrayColumn, MapFromArraysColumn, MapColumn
-from pysparkling.sql.expressions.dates import AddMonths, CurrentDate
+from pysparkling.sql.expressions.dates import AddMonths, CurrentDate, CurrentTimestamp
 from pysparkling.sql.expressions.mappers import CaseWhen, Rand, CreateStruct, Grouping, GroupingID, Coalesce, \
     InputFileName, IsNaN, MonotonicallyIncreasingID, NaNvl, Randn, SparkPartitionID, Sqrt, Abs, Acos, Asin, Atan, Atan2, \
     Bin, Cbrt, Ceil, Conv, Cos, Cosh, Exp, ExpM1, Factorial, Floor, Greatest, Hex, Unhex, Hypot, Least, Log, Log10, \
@@ -1525,3 +1525,10 @@ def current_date():
     :rtype: Column
     """
     return col(CurrentDate())
+
+
+def current_timestamp():
+    """
+    :rtype: Column
+    """
+    return col(CurrentTimestamp())
