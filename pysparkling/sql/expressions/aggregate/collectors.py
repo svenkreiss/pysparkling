ffa88be348852acdddd8a4660925548441f340bf
tools4origins <tools4origins@gmail.com>
2020-10-04 12:03:02 +0200

    Implement SumDistinct

--- pysparkling/sql/expressions/aggregate/collectors.py
+++ pysparkling/sql/expressions/aggregate/collectors.py
@@ -37,3 +37,23 @@ class CollectSet(Aggregation):
 
     def __str__(self):
         return "collect_set({0})".format(self.column)
+
+
+class SumDistinct(Aggregation):
+    def __init__(self, column):
+        super(SumDistinct, self).__init__(column)
+        self.column = column
+        self.items = set()
+
+    def merge(self, row, schema):
+        self.items.add(self.column.eval(row, schema))
+
+    def mergeStats(self, other, schema):
+        self.items |= other.items
+
+    def eval(self, row, schema):
+        return sum(self.items)
+
+    def __str__(self):
+        return "sum_distinct({0})".format(self.column)
+
