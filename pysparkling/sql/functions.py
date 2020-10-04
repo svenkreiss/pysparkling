9d11433e068ab91e38b8906b804e4cfdfcbfb269
tools4origins <tools4origins@gmail.com>
2020-10-04 15:35:45 +0200

    Implement lpad function

--- pysparkling/sql/functions.py
+++ pysparkling/sql/functions.py
@@ -14,7 +14,7 @@ from pysparkling.sql.expressions.mappers import CaseWhen, Rand, CreateStruct, Gr
     FormatNumber, Length, Lower
 from pysparkling.sql.expressions.literals import Literal
 from pysparkling.sql.expressions.operators import IsNull, BitwiseNot, Pow, Pmod
-from pysparkling.sql.expressions.strings import InitCap, StringInStr, Levenshtein, StringLocate
+from pysparkling.sql.expressions.strings import InitCap, StringInStr, Levenshtein, StringLocate, StringLPad
 
 
 def col(colName):
@@ -1312,3 +1312,12 @@ def locate(substr, str, pos=1):
     :rtype: Column
     """
     return col(StringLocate(substr, parse(str), pos))
+
+
+# noinspection PyShadowingBuiltins
+# pylint: disable=W0622
+def lpad(str, len, pad):
+    """
+    :rtype: Column
+    """
+    return col(StringLPad(parse(str), len, pad))
