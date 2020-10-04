cbe36ddb159d5621f92daba609a1007d2ca93568
tools4origins <tools4origins@gmail.com>
2020-10-04 17:10:49 +0200

    Implement GroupedData.pivot

--- pysparkling/sql/group.py
+++ pysparkling/sql/group.py
@@ -71,3 +71,52 @@ class GroupedData(object):
 
     def sum(self, *cols):
         return self.agg(*(sum(parse(col)) for col in cols))
+
+    def pivot(self, pivot_col, values=None):
+        """
+        >>> from pysparkling import Context, Row
+        >>> from pysparkling.sql.session import SparkSession
+        >>> from pysparkling.sql.functions import col, avg, sum
+        >>> sc = Context()
+        >>> spark = SparkSession(sc)
+        >>> df4 = sc.parallelize([Row(course="dotNET", year=2012, earnings=10000),
+        ...                        Row(course="Java",   year=2012, earnings=20000),
+        ...                        Row(course="dotNET", year=2012, earnings=5000),
+        ...                        Row(course="dotNET", year=2013, earnings=48000),
+        ...                        Row(course="Java",   year=2013, earnings=30000)]).toDF()
+        >>> df4.groupBy("year").pivot("course", ["dotNET", "Java"]).sum("earnings").collect()
+        [Row(year=2012, dotNET=15000, Java=20000), Row(year=2013, dotNET=48000, Java=30000)]
+        >>> df4.groupBy("year").pivot("course").sum("earnings").collect()
+        [Row(year=2012, Java=20000, dotNET=15000), Row(year=2013, Java=30000, dotNET=48000)]
+        >>> df4.groupBy("year").pivot("course", ["dotNET"]).sum("earnings").collect()
+        [Row(year=2012, dotNET=15000), Row(year=2013, dotNET=48000)]
+        >>> df4.groupBy("year").pivot("course").agg(sum("earnings")).show()
+        +----+-----+------+
+        |year| Java|dotNET|
+        +----+-----+------+
+        |2012|20000| 15000|
+        |2013|30000| 48000|
+        +----+-----+------+
+        >>> df4.groupBy("year").pivot("course").agg(sum("earnings"), avg("earnings")).show()
+        +----+------------------+------------------+--------------------+--------------------+
+        |year|Java_sum(earnings)|Java_avg(earnings)|dotNET_sum(earnings)|dotNET_avg(earnings)|
+        +----+------------------+------------------+--------------------+--------------------+
+        |2012|             20000|           20000.0|               15000|              7500.0|
+        |2013|             30000|           30000.0|               48000|             48000.0|
+        +----+------------------+------------------+--------------------+--------------------+
+        """
+        jgd = self._jgd.pivot(parse(pivot_col), values)
+        return GroupedData(jgd, self._df)
+
+    # pylint: disable=W0511
+    # todo: implement apply()
+    # def apply(self, udf):
+    #     # Columns are special because hasattr always return True
+    #     if isinstance(udf, Column) or not hasattr(udf, 'func') \
+    #             or udf.evalType != PythonEvalType.SQL_GROUPED_MAP_PANDAS_UDF:
+    #         raise ValueError("Invalid udf: the udf argument must be a pandas_udf of type "
+    #                          "GROUPED_MAP.")
+    #     df = self._df
+    #     udf_column = udf(*[df[col] for col in df.columns])
+    #     jdf = self._jgd.flatMapGroupsInPandas(udf_column._jc.expr())
+    #     return DataFrame(jdf, self.sql_ctx)
