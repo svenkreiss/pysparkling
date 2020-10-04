7a674a9510a2de5e63feeae6867a38a861c31a2b
tools4origins <tools4origins@gmail.com>
2020-10-04 17:43:56 +0200

    Add test on when function

--- pysparkling/sql/functions.py
+++ pysparkling/sql/functions.py
@@ -263,20 +263,20 @@ def corr(column1, column2):
 
 def when(condition, value):
     """
-    # >>> from pysparkling import Context, Row
-    # >>> from pysparkling.sql.session import SparkSession
-    # >>> spark = SparkSession(Context())
-    # >>> df = spark.createDataFrame(
-    # ...    [Row(age=2, name='Alice'), Row(age=5, name='Bob'), Row(age=4, name='Lisa')]
-    # ... )
-    # >>> df.select(df.name, when(df.age > 4, -1).when(df.age < 3, 1).otherwise(0)).show()
-    # +-----+------------------------------------------------------------+
-    # | name|CASE WHEN (age > 4) THEN -1 WHEN (age < 3) THEN 1 ELSE 0 END|
-    # +-----+------------------------------------------------------------+
-    # |Alice|                                                           1|
-    # |  Bob|                                                          -1|
-    # | Lisa|                                                           0|
-    # +-----+------------------------------------------------------------+
+    >>> from pysparkling import Context, Row
+    >>> from pysparkling.sql.session import SparkSession
+    >>> spark = SparkSession(Context())
+    >>> df = spark.createDataFrame(
+    ...    [Row(age=2, name='Alice'), Row(age=5, name='Bob'), Row(age=4, name='Lisa')]
+    ... )
+    >>> df.select(df.name, when(df.age > 4, -1).when(df.age < 3, 1).otherwise(0)).show()
+    +-----+------------------------------------------------------------+
+    | name|CASE WHEN (age > 4) THEN -1 WHEN (age < 3) THEN 1 ELSE 0 END|
+    +-----+------------------------------------------------------------+
+    |Alice|                                                           1|
+    |  Bob|                                                          -1|
+    | Lisa|                                                           0|
+    +-----+------------------------------------------------------------+
 
     :rtype: Column
     """
