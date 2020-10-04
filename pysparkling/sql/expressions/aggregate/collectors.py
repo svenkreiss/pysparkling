48142ad374cb5272be4dce6cda41ef5dbe7a3e85
tools4origins <tools4origins@gmail.com>
2020-10-04 12:04:49 +0200

    Implement First

--- pysparkling/sql/expressions/aggregate/collectors.py
+++ pysparkling/sql/expressions/aggregate/collectors.py
@@ -57,3 +57,26 @@ class SumDistinct(Aggregation):
     def __str__(self):
         return "sum_distinct({0})".format(self.column)
 
+
+class First(Aggregation):
+    _sentinel = object()
+
+    def __init__(self, column, ignore_nulls):
+        super(First, self).__init__(column)
+        self.column = column
+        self.value = self._sentinel
+        self.ignore_nulls = ignore_nulls
+
+    def merge(self, row, schema):
+        if self.value is First._sentinel or (self.ignore_nulls and self.value is None):
+            self.value = self.column.eval(row, schema)
+
+    def mergeStats(self, other, schema):
+        if self.value is First._sentinel or (self.ignore_nulls and self.value is None):
+            self.value = other.value
+
+    def eval(self, row, schema):
+        return self.value if self.value is not First._sentinel else None
+
+    def __str__(self):
+        return "first({0}, {1})".format(self.column, str(self.ignore_nulls).lower())
