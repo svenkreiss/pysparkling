1ab53f782d1102d5cadf749547d30a55927fe341
tools4origins <tools4origins@gmail.com>
2020-10-04 15:51:12 +0200

    Implement element_at function

--- pysparkling/sql/functions.py
+++ pysparkling/sql/functions.py
@@ -7,7 +7,7 @@ from pysparkling.sql.expressions.aggregate.covariance_aggregations import Corr, 
 from pysparkling.sql.expressions.aggregate.stat_aggregations import Count, Avg, Kurtosis, Max, Min, Skewness, \
     StddevSamp, StddevPop, Sum, VarSamp, VarPop
 from pysparkling.sql.expressions.arrays import ArrayColumn, MapFromArraysColumn, MapColumn, ArrayContains, \
-    ArraysOverlap, Slice, ArrayJoin, ArrayPosition
+    ArraysOverlap, Slice, ArrayJoin, ArrayPosition, ElementAt
 from pysparkling.sql.expressions.dates import AddMonths, CurrentDate, CurrentTimestamp, DateFormat, DateAdd, DateSub, \
     DateDiff, Year, Quarter, Month, DayOfWeek, DayOfMonth, DayOfYear, Hour, LastDay, Minute, MonthsBetween, NextDay, \
     Second, WeekOfYear, FromUnixTime, UnixTimestamp, ParseToTimestamp, ParseToDate, TruncDate, TruncTimestamp, \
@@ -2111,3 +2111,10 @@ def array_position(column, value):
     :rtype: Column
     """
     return col(ArrayPosition(column, value))
+
+
+def element_at(column, value):
+    """
+    :rtype: Column
+    """
+    return col(ElementAt(column, value))
