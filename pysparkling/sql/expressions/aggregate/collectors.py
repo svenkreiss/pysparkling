f0aacd50d7a6bbd10ecdea09eb02c9003f12445c
tools4origins <tools4origins@gmail.com>
2020-10-04 12:02:45 +0200

    Implement CollectList

--- pysparkling/sql/expressions/aggregate/collectors.py
+++ pysparkling/sql/expressions/aggregate/collectors.py
@@ -1,6 +1,25 @@
 from pysparkling.sql.expressions.aggregate.aggregations import Aggregation
 
 
+class CollectList(Aggregation):
+    def __init__(self, column):
+        super(CollectList, self).__init__(column)
+        self.column = column
+        self.items = []
+
+    def merge(self, row, schema):
+        self.items.append(self.column.eval(row, schema))
+
+    def mergeStats(self, other, schema):
+        self.items += other.items
+
+    def eval(self, row, schema):
+        return self.items
+
+    def __str__(self):
+        return "collect_list({0})".format(self.column)
+
+
 class CollectSet(Aggregation):
     def __init__(self, column):
         super(CollectSet, self).__init__(column)
