d837bcf7e6710fac00a0984a250f9e30d085be4b
tools4origins <tools4origins@gmail.com>
2020-10-04 15:08:20 +0200

    Implement asc_nulls_first function

--- pysparkling/sql/functions.py
+++ pysparkling/sql/functions.py
@@ -58,6 +58,30 @@ def asc(columnName):
     return parse(columnName).asc()
 
 
+def asc_nulls_first(columnName):
+    """
+    :rtype: Column
+
+    >>> from pysparkling import Context, Row
+    >>> from pysparkling.sql.session import SparkSession
+    >>> from pysparkling.sql.functions import when, col
+    >>> spark = SparkSession(Context())
+    >>> df = spark.range(5).withColumn(
+    ...   "order", when(col('id')%2 == 0, col('id'))
+    ... ).orderBy(asc_nulls_first("order")).show()
+    +---+-----+
+    | id|order|
+    +---+-----+
+    |  1| null|
+    |  3| null|
+    |  0|    0|
+    |  2|    2|
+    |  4|    4|
+    +---+-----+
+    """
+    return parse(columnName).asc_nulls_first()
+
+
 def when(condition, value):
     """
     # >>> from pysparkling import Context, Row
