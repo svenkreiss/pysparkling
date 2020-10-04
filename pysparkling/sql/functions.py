c95165b19bc6952da98ceaa915fb80778e868729
tools4origins <tools4origins@gmail.com>
2020-10-04 15:13:08 +0200

    Implement kurtosis function

--- pysparkling/sql/functions.py
+++ pysparkling/sql/functions.py
@@ -2,7 +2,7 @@ from pysparkling.sql.column import Column, parse
 from pysparkling.sql.expressions.aggregate.collectors import CollectSet, ApproxCountDistinct, CollectList, \
     CountDistinct, First
 from pysparkling.sql.expressions.aggregate.covariance_aggregations import Corr, CovarPop, CovarSamp
-from pysparkling.sql.expressions.aggregate.stat_aggregations import Count, Avg
+from pysparkling.sql.expressions.aggregate.stat_aggregations import Count, Avg, Kurtosis
 from pysparkling.sql.expressions.arrays import ArrayColumn, MapFromArraysColumn
 from pysparkling.sql.expressions.mappers import CaseWhen, Rand, CreateStruct, Grouping, GroupingID
 from pysparkling.sql.expressions.literals import Literal
@@ -465,3 +465,10 @@ def grouping_id(*exprs):
     """
     cols = [parse(e) for e in exprs]
     return col(GroupingID(cols))
+
+
+def kurtosis(e):
+    """
+    :rtype: Column
+    """
+    return col(Kurtosis(column=parse(e)))
