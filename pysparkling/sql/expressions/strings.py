7167aaa768ab29c14d68a07f029d8a573aecbdd7
tools4origins <tools4origins@gmail.com>
2020-10-04 12:46:42 +0200

    Implement StringLocate expression

--- pysparkling/sql/expressions/strings.py
+++ pysparkling/sql/expressions/strings.py
@@ -41,3 +41,24 @@ class StringInStr(Expression):
             self.substr,
             self.column
         )
+
+
+class StringLocate(Expression):
+    def __init__(self, substr, column, pos):
+        super(StringLocate, self).__init__(column)
+        self.substr = substr
+        self.column = column
+        self.start = pos - 1
+
+    def eval(self, row, schema):
+        value = self.column.cast(StringType()).eval(row, schema)
+        if self.substr not in value[self.start:]:
+            return 0
+        return value.index(self.substr, self.start) + 1
+
+    def __str__(self):
+        return "locate({0}, {1}{2})".format(
+            self.substr,
+            self.column,
+            ", {0}".format(self.start) if self.start is not None else ""
+        )
