9f01eb98f04ced924b78abad1d55bdda5d729d49
tools4origins <tools4origins@gmail.com>
2020-10-04 15:52:08 +0200

    Implement array_intersect function

--- pysparkling/sql/functions.py
+++ pysparkling/sql/functions.py
@@ -7,7 +7,7 @@ from pysparkling.sql.expressions.aggregate.covariance_aggregations import Corr, 
 from pysparkling.sql.expressions.aggregate.stat_aggregations import Count, Avg, Kurtosis, Max, Min, Skewness, \
     StddevSamp, StddevPop, Sum, VarSamp, VarPop
 from pysparkling.sql.expressions.arrays import ArrayColumn, MapFromArraysColumn, MapColumn, ArrayContains, \
-    ArraysOverlap, Slice, ArrayJoin, ArrayPosition, ElementAt, ArraySort, ArrayRemove, ArrayDistinct
+    ArraysOverlap, Slice, ArrayJoin, ArrayPosition, ElementAt, ArraySort, ArrayRemove, ArrayDistinct, ArrayIntersect
 from pysparkling.sql.expressions.dates import AddMonths, CurrentDate, CurrentTimestamp, DateFormat, DateAdd, DateSub, \
     DateDiff, Year, Quarter, Month, DayOfWeek, DayOfMonth, DayOfYear, Hour, LastDay, Minute, MonthsBetween, NextDay, \
     Second, WeekOfYear, FromUnixTime, UnixTimestamp, ParseToTimestamp, ParseToDate, TruncDate, TruncTimestamp, \
@@ -2139,3 +2139,10 @@ def array_distinct(e):
     :rtype: Column
     """
     return col(ArrayDistinct(e))
+
+
+def array_intersect(col1, col2):
+    """
+    :rtype: Column
+    """
+    return col(ArrayIntersect(parse(col1), parse(col2)))
