96292b7b0a6ee88972aac560f86c24de03ce3aeb
tools4origins <tools4origins@gmail.com>
2020-10-04 15:57:47 +0200

    Implement map_from_entries function

--- pysparkling/sql/functions.py
+++ pysparkling/sql/functions.py
@@ -19,7 +19,7 @@ from pysparkling.sql.expressions.mappers import CaseWhen, Rand, CreateStruct, Gr
     Bin, Cbrt, Ceil, Conv, Cos, Cosh, Exp, ExpM1, Factorial, Floor, Greatest, Hex, Unhex, Hypot, Least, Log, Log10, \
     Log1p, Log2, Rint, Round, Bround, Signum, Sin, Sinh, Tan, Tanh, ToDegrees, ToRadians, Ascii, Base64, ConcatWs, \
     FormatNumber, Length, Lower, RegExpExtract, RegExpReplace, UnBase64, StringSplit, SubstringIndex, Upper, Concat, \
-    Reverse, MapKeys, MapValues, MapEntries
+    Reverse, MapKeys, MapValues, MapEntries, MapFromEntries
 from pysparkling.sql.expressions.literals import Literal
 from pysparkling.sql.expressions.operators import IsNull, BitwiseNot, Pow, Pmod, Substring
 from pysparkling.sql.expressions.strings import InitCap, StringInStr, Levenshtein, StringLocate, StringLPad, \
@@ -2307,3 +2307,10 @@ def map_entries(e):
     :rtype: Column
     """
     return col(MapEntries(e))
+
+
+def map_from_entries(e):
+    """
+    :rtype: Column
+    """
+    return col(MapFromEntries(e))
