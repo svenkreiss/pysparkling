993d31cedf7c37e5eebacdd4c7d4395551826de9
tools4origins <tools4origins@gmail.com>
2020-10-04 16:36:56 +0200

    Add an aggregation that, when called, write in a folder

--- pysparkling/sql/internal_utils/writers.py
+++ pysparkling/sql/internal_utils/writers.py
@@ -1,3 +1,7 @@
+from pysparkling import Row
+from pysparkling.sql.expressions.aggregate.aggregations import Aggregation
+from pysparkling.sql.expressions.mappers import StarOperator
+from pysparkling.sql.functions import col
 from pysparkling.sql.internal_utils.readwrite import to_option_stored_value
 
 
@@ -49,3 +53,49 @@ class InternalWriter(object):
             self._sort_col_names
         ).save()
 
+
+class WriteInFolder(Aggregation):
+    """
+    This use the computation engine of pysparkling to write the values in a folder.
+
+    It's behaviour is similar to a collect_list except that items are written
+    in a folder instead of being returned:
+
+    Its evaluation only return the number of items written while writing them
+    using the writer given in the constructor, more specifically its write method.
+
+    Pre-formatting is done as defined by writer.preformat during the merge phase.
+
+    """
+
+    def __init__(self, writer):
+        super(WriteInFolder, self).__init__()
+        self.column = col(StarOperator())
+        self.writer = writer
+        self.ref_value = None
+        self.items = []
+
+    def merge(self, row, schema):
+        row_value = self.column.eval(row, schema)
+        if self.ref_value is None:
+            ref_value = Row(*row_value)
+            ref_value.__fields__ = schema.names
+            self.ref_value = ref_value
+        self.items.append(
+            self.writer.preformat(row_value, schema)
+        )
+
+    def mergeStats(self, other, schema):
+        self.items += other.items
+        if self.ref_value is None:
+            self.ref_value = other.ref_value
+
+    def eval(self, row, schema):
+        return self.writer.write(
+            self.items,
+            self.ref_value,
+            self.pre_evaluation_schema
+        )
+
+    def __str__(self):
+        return "write_in_folder({0})".format(self.column)
