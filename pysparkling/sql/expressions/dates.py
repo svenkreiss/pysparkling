a69add2fdabc669c8ecc58f3de97f2713c2a328f
tools4origins <tools4origins@gmail.com>
2020-10-04 12:17:00 +0200

    Implement DateAdd Expression

--- pysparkling/sql/expressions/dates.py
+++ pysparkling/sql/expressions/dates.py
@@ -1,3 +1,5 @@
+import datetime
+
 from dateutil.relativedelta import relativedelta
 
 from pysparkling.sql.expressions.expressions import Expression
@@ -16,3 +18,17 @@ class AddMonths(Expression):
 
     def __str__(self):
         return "add_months({0}, {1})".format(self.start_date, self.num_months)
+
+
+class DateAdd(Expression):
+    def __init__(self, start_date, num_days):
+        super(DateAdd, self).__init__(start_date)
+        self.start_date = start_date
+        self.num_days = num_days
+        self.timedelta = datetime.timedelta(days=num_days)
+
+    def eval(self, row, schema):
+        return self.start_date.cast(DateType()).eval(row, schema) + self.timedelta
+
+    def __str__(self):
+        return "date_add({0}, {1})".format(self.start_date, self.num_days)
