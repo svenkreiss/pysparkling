04a50b3e61fe3b2ca61cd646ce378539f0191b58
Steven Van Ingelgem <steven@vaningelgem.be>
2021-01-27 22:58:03 +0100

    Removed another py2 compatibility block.

--- pysparkling/context.py
+++ pysparkling/context.py
@@ -21,12 +21,8 @@ from .partition import Partition
 from .rdd import RDD, EmptyRDD
 from .task_context import TaskContext
 
-# Python 2 compatibility
 if not hasattr(time, 'perf_counter'):
-    try:  # Python 2
-        time.perf_counter = time.clock
-    except AttributeError:  # Unexpected
-        raise Exception("`time`should contain either clock (py2) or perf_counter (py3)")
+    raise Exception("`time`should contain perf_counter")
 
 
 log = logging.getLogger(__name__)
