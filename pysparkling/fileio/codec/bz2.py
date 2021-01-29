cd7bfe03b86efc76e9790af3510c89c346fde7d4
Steven Van Ingelgem <steven@vaningelgem.be>
2021-01-29 07:01:17 +0100

    Looks nicer this way.

--- pysparkling/fileio/codec/bz2.py
+++ pysparkling/fileio/codec/bz2.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
 import bz2
+import io
 import logging
-from io import BytesIO
 
 from .codec import Codec
 
@@ -13,7 +13,7 @@ class Bz2(Codec):
     """Implementation of :class:`.Codec` for bz2 compression."""
 
     def compress(self, stream):
-        return BytesIO(bz2.compress(b''.join(stream)))
+        return io.BytesIO(bz2.compress(b''.join(stream)))
 
     def decompress(self, stream):
-        return BytesIO(bz2.decompress(stream.read()))
+        return io.BytesIO(bz2.decompress(stream.read()))
