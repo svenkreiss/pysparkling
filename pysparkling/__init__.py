bab0ffed87f3c7223f3c3e38b3fd28a6bb37f965
Steven Van Ingelgem <steven@vaningelgem.be>
2021-01-30 13:37:33 +0100

    Fix for import hell.

--- pysparkling/__init__.py
+++ pysparkling/__init__.py
@@ -5,13 +5,16 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+# isort: off
+from .sql.types import Row
+# isort: on
+
 from . import exceptions, fileio, streaming
 from .accumulators import Accumulator, AccumulatorParam
 from .broadcast import Broadcast
 from .cache_manager import CacheManager, TimedCacheManager
 from .context import Context
 from .rdd import RDD
-from .sql.types import Row
 from .stat_counter import StatCounter
 from .storagelevel import StorageLevel
 
